25c4b7ebed3fa038ebf777108ee343902f49675b
Chad Autry <chad.autry@gmail.com>
2017-06-24 08:43:19 -0500

    Refactor authorization to controller

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+ router.use(bodyParser.urlencoded({ extended: true }));
+ 
++//Use the authorization middleware on every method of this controller
++router.use(authMiddleware);
 var path = require('path');
 var qs = require('querystring');
 
@@ -12,70 +16,15 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 var mongoose = require('mongoose');
 var request = require('request');
-
+var AuthorizationController = require('./controllers/Authorization');
 var config = require('../config');
 
 
 var app = express();
 
-app.use(cors());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-
-/*
- |--------------------------------------------------------------------------
- | Generate JSON Web Token
- |--------------------------------------------------------------------------
- */
-function createJWT(user) {
-  var payload = {
-    sub: user._id,
-    iat: moment().unix(),
-    exp: moment().add(14, 'days').unix(),
-    displayName: user.displayName
-  };
-  return jwt.encode(payload, config.JWT_TOKEN_SECRET);
-}
-
-/*
- |--------------------------------------------------------------------------
- | Login with Google
- |--------------------------------------------------------------------------
- */
-app.get('/backend/googleAuth', function(req, res) {
-  var accessTokenUrl = 'https://accounts.google.com/o/oauth2/token';
-  var peopleApiUrl = 'https://www.googleapis.com/plus/v1/people/me/openIdConnect';
-  var params = {
-    code: req.query.code,
-    client_id: config.GOOGLE_CLIENT_ID,
-    client_secret: config.GOOGLE_AUTH_SECRET,
-    redirect_uri: config.GOOGLE_REDIRECT_URI,
-    grant_type: 'authorization_code'
-  };
-
-  // Step 1. Exchange authorization code for access token.
-  request.post(accessTokenUrl, { json: true, form: params }, function(err, response, token) {
-    var accessToken = token.access_token;
-    var headers = { Authorization: 'Bearer ' + accessToken };
-
-    // Step 2. Retrieve profile information about the current user.
-    request.get({ url: peopleApiUrl, headers: headers, json: true }, function(err, response, profile) {
-      if (profile.error) {
-        return res.status(500).send({message: profile.error.message});
-      }
-      var user = {};
-      user.google = profile.sub;
-      user.picture = profile.picture.replace('sz=50', 'sz=200');
-      user.displayName = profile.name;
-      user.name = profile.name;
-      var token = createJWT(user);
-      var responseString = "<html><body><div id=\"token\">" + token + "</div></body></html>";
-      res.send(responseString);
-    });
-  });
-});
+app.use('/backend/auth', AuthorizationController);
 
 /*
  |--------------------------------------------------------------------------
